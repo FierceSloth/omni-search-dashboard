7b8a1f9261c03dcee0542547170ac95cd588d14e
Dastan Hairushev <hairushevdastan@gmail.com>
2026-04-29 21:37:15 +0500

    style: setup global design system

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,13 @@ import { Component, type ReactNode } from 'react';
 export class App extends Component {
   public render(): ReactNode {
     return (
-      <div className="app-container">
-        <h1>Omni Search Dashboard</h1>
-      </div>
+      <>
+        <div className="ambient-light"></div>
+
+        <div className="container">
+          <h1>Omni Search Dashboard</h1>
+        </div>
+      </>
     );
   }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '@/app';
 
+import '@/styles/style.scss';
+
 const rootElement = document.querySelector('#root');
 
 if (rootElement) {
