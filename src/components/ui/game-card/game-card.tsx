8409129a64f679dc77a19bb2f98bdf262d9fb286
Dastan Hairushev <hairushevdastan@gmail.com>
2026-04-30 17:47:54 +0500

    feat: add game card component

---
+++ src/components/ui/game-card/game-card.tsx
@@ -0,0 +1,43 @@
+import { Component, type ReactNode } from 'react';
+
+import styles from './game-card.module.scss';
+
+interface IProps {
+  title: string;
+  imageUrl: string;
+  genre: string;
+  rating?: number;
+  released?: string;
+}
+
+export class GameCard extends Component<IProps> {
+  public render(): ReactNode {
+    const { title, released, genre, imageUrl, rating } = this.props;
+
+    let date = 'TBA';
+
+    if (released) {
+      date = new Date(released).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      });
+    }
+
+    return (
+      <div className={styles.gameCard}>
+        <img className={styles.image} src={imageUrl} alt={title} />
+
+        <div className={styles.content}>
+          <div className={styles.meta}>
+            <div className={styles.genre}>{genre}</div>
+            {rating && <div className={styles.rating}>{rating} ★</div>}
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+
+          <p className={styles.date}>{date}</p>
+        </div>
+      </div>
+    );
+  }
+}
