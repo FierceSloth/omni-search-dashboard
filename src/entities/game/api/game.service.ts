95283c596c51d42df5f16e806e9f71f6de8e8858
Dastan Hairushev <hairushevdastan@gmail.com>
2026-05-15 17:44:29 +0500

    feat: implement pagination support in GameService

--- src/entities/game/api/game.service.ts
+++ src/entities/game/api/game.service.ts
@@ -2,11 +2,24 @@ import type { IGamesResponse } from '@/entities/game/model/responses';
 import type { IGameCardDTO } from '@/entities/game/model/types';
 import { RawgClient } from '@/shared/api/rawg-client';
 
+export interface ISearchGamesResponse {
+  games: IGameCardDTO[];
+  totalPages: number;
+}
+
 export class GameService {
-  public static async searchGames(query: string): Promise<IGameCardDTO[]> {
-    const endpoint = query ? `/games?search=${encodeURIComponent(query)}` : `/games`;
+  private static readonly PAGE_SIZE = 20;
+
+  public static async searchGames(query: string, page: number = 1): Promise<ISearchGamesResponse> {
+    const searchParameter = query ? `&search=${encodeURIComponent(query)}` : '';
+    const endpoint = `/games?page=${page}&page_size=${this.PAGE_SIZE}${searchParameter}`;
 
     const data = await RawgClient.fetchData<IGamesResponse>(endpoint);
-    return data.results;
+    const totalPages = Math.ceil(data.count / this.PAGE_SIZE);
+
+    return {
+      games: data.results,
+      totalPages: totalPages,
+    };
   }
 }
