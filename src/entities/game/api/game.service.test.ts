be9796a82539c8a7b369bf31f5bfcd8dcd8fd0d0
Dastan Hairushev <hairushevdastan@gmail.com>
2026-05-18 19:09:56 +0500

    test: update game-service tests

--- src/entities/game/api/game.service.test.ts
+++ src/entities/game/api/game.service.test.ts
@@ -1,5 +1,5 @@
 import { GameService } from '@/entities/game/api/game.service';
-import { mockValidApiResponse } from '@/entities/game/model/mocks/responses.mock';
+import { mockGameDetailsResponse, mockValidApiResponse } from '@/entities/game/model/mocks/responses.mock';
 import { RawgClient } from '@/shared/api/rawg-client';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
@@ -8,47 +8,75 @@ describe('GameService', () => {
     vi.resetAllMocks();
   });
 
-  const expectedTotalPages = Math.ceil(mockValidApiResponse.count / 20);
+  describe('searchGames', () => {
+    const expectedTotalPages = Math.ceil(mockValidApiResponse.count / 20);
 
-  it('should call RawgClient with search query and return results', async () => {
-    const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
+    it('should call RawgClient with search query and return results', async () => {
+      const testQuery = 'GTA V';
+      const expectedEndpoint = `/games?page=1&page_size=20&search=${encodeURIComponent(testQuery)}`;
+      const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
 
-    const result = await GameService.searchGames('GTA V');
+      const result = await GameService.searchGames(testQuery);
 
-    expect(fetchSpy).toHaveBeenCalledWith('/games?page=1&page_size=20&search=GTA%20V');
+      expect(fetchSpy).toHaveBeenCalledWith(expectedEndpoint);
+      expect(result).toEqual({
+        games: mockValidApiResponse.results,
+        totalPages: expectedTotalPages,
+      });
+    });
 
-    expect(result).toEqual({
-      games: mockValidApiResponse.results,
-      totalPages: expectedTotalPages,
+    it('should call RawgClient without search query with base endpoint', async () => {
+      const emptyQuery = '';
+      const expectedEndpoint = '/games?page=1&page_size=20';
+      const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
+
+      const result = await GameService.searchGames(emptyQuery);
+
+      expect(fetchSpy).toHaveBeenCalledWith(expectedEndpoint);
+      expect(result).toEqual({
+        games: mockValidApiResponse.results,
+        totalPages: expectedTotalPages,
+      });
+    });
+
+    it('should call RawgClient with specific page', async () => {
+      const testQuery = 'Mario';
+      const testPage = 3;
+      const expectedEndpoint = `/games?page=${testPage}&page_size=20&search=${encodeURIComponent(testQuery)}`;
+      const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
+
+      const result = await GameService.searchGames(testQuery, testPage);
+
+      expect(fetchSpy).toHaveBeenCalledWith(expectedEndpoint);
+      expect(result.games).toEqual(mockValidApiResponse.results);
+    });
+
+    it('should throw an error if RawgClient throws an error', async () => {
+      const networkError = new Error('HTTP error!');
+      vi.spyOn(RawgClient, 'fetchData').mockRejectedValue(networkError);
+
+      await expect(GameService.searchGames('')).rejects.toThrow();
     });
   });
 
-  it('should call RawgClient without search query with base endpoint', async () => {
-    const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
+  describe('getGameById', () => {
+    it('should call RawgClient with correct endpoint and return game details', async () => {
+      const testId = mockGameDetailsResponse.id;
+      const expectedEndpoint = `/games/${testId}`;
+      const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockGameDetailsResponse);
 
-    const result = await GameService.searchGames('');
+      const result = await GameService.getGameById(testId);
 
-    expect(fetchSpy).toHaveBeenCalledWith('/games?page=1&page_size=20');
-
-    expect(result).toEqual({
-      games: mockValidApiResponse.results,
-      totalPages: expectedTotalPages,
+      expect(fetchSpy).toHaveBeenCalledWith(expectedEndpoint);
+      expect(result).toEqual(mockGameDetailsResponse);
     });
-  });
 
-  it('should call RawgClient with specific page', async () => {
-    const fetchSpy = vi.spyOn(RawgClient, 'fetchData').mockResolvedValue(mockValidApiResponse);
+    it('should throw an error if RawgClient fails to fetch game details', async () => {
+      const testId = mockGameDetailsResponse.id;
+      const expectedError = new Error('Not found');
+      vi.spyOn(RawgClient, 'fetchData').mockRejectedValue(expectedError);
 
-    const result = await GameService.searchGames('Mario', 3);
-
-    expect(fetchSpy).toHaveBeenCalledWith('/games?page=3&page_size=20&search=Mario');
-    expect(result.games).toEqual(mockValidApiResponse.results);
-  });
-
-  it('should throw an error if RawgClient throws an error', async () => {
-    const networkError = new Error('HTTP error!');
-    vi.spyOn(RawgClient, 'fetchData').mockRejectedValue(networkError);
-
-    await expect(GameService.searchGames('')).rejects.toThrow();
+      await expect(GameService.getGameById(testId)).rejects.toThrow(expectedError);
+    });
   });
 });
