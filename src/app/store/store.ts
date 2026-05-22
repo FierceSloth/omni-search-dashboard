93f3c72efaeb7d812d2d3881044251ce6fdc8dd2
Dastan Hairushev <hairushevdastan@gmail.com>
2026-05-22 20:59:23 +0500

    feat: configure redux store and typed hooks

---
+++ src/app/store/store.ts
@@ -0,0 +1,11 @@
+import { selectedCardsReducer } from '@/features/selected-cards';
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    selectedCards: selectedCardsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
