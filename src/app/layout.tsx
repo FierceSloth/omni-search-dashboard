334cd5eb665722e7510c4551b4b45dec40e6b967
Dastan Hairushev <hairushevdastan@gmail.com>
2026-06-19 22:36:07 +0500

    feat: add redux and theme providers to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
+import { StoreProvider } from '@/app/_providers/store.provider';
+import { ThemeProvider } from '@/shared/lib/context/theme';
 import '@app/_styles/style.scss';
 import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }): ReactNode {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StoreProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </StoreProvider>
+      </body>
     </html>
   );
 }
